3d84f0dfafe826e59ddc1ef8533baa68decaee29
dbmediatech <sharonrojamajjib13@gmail.com>
2023-10-20 17:21:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Starship from './components/Starship/Starship';
 import { FaTrophy } from 'react-icons/fa';
+import Video from './images/starwars.mp4';
 import './App.scss';
 
 const App = () => {
@@ -12,7 +13,6 @@ const App = () => {
       const starshipsData = response.data.results
         .filter((starship) => starship.crew <= 10)
         .sort((a, b) => a.crew - b.crew);
-        console.log(response.data.results)
 
       const mostFilmsStarship = starshipsData.reduce((prev, current) =>
         prev.films.length > current.films.length ? prev : current
@@ -24,13 +24,15 @@ const App = () => {
 
   return (
     <div className="starship-main-container">
+        <video autoPlay loop muted playsInline className="starship-video">
+          <source src={Video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       <div className='starship-inner-container'>
         <div className='starship-logo-container'>
           <img src={require('./images/logo.png')} alt='logo' />
         </div>
         <p className='starship-content'>
-          Results are  filtered to starships with a crew size less than 10 and sorted by crew size.
-          <br />
           The starship that has featured in most films will show a <span className='starship-content-trophy'><FaTrophy className="star-icon" /></span>
         </p>
         <div className='starship-list-container'>
--- src/components/Starship/Starship.js
+++ src/components/Starship/Starship.js
@@ -10,12 +10,8 @@ const Starship = ({ starship }) => {
             <h2 className='startship-title'>{starship.name}
                 <span className='starship-trophy'>{starship.mostFilms ? <FaTrophy className="star-icon" /> : null}</span>
             </h2>
-            <p className='starship-model-title'>Model:</p>
-            <p className='starship-model-number'>{starship.model}</p>
-        </div>
-        <div className='starship-film-container'>
-            <p className='starship-films-title'>No. of Films: </p>
-            <p className='starship-films-list'>{starship.films.length}</p>
+            <p className='starship-model-title'>Model: {starship.model}</p>
+            <p className='starship-films-title'>No. of Films: {starship.films.length}</p>
         </div>
         <div className='starship-img-container'>
             <img
